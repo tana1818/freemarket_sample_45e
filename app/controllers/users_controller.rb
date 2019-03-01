42bf40091b1f0131c184ca27230b10128cd33f36
teru0421 <kazuhiro.terui4036@gmail.com>
2019-03-01 17:03:46 +0900

    Try 2

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+
+  def login
+  end
+
+  def logout
+  end
+
+  def credit
+  end
+
+  def creditcreate
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'users#login'
+  get  '/users' => 'users#login'
+  get  '/users/logout' => 'users#logout'
+  get  '/users/show' => 'users#show'
+  get  '/users/credit' => 'users#credit'
+  get  '/users/creditcreate' => 'users#creditcreate'
+
 end

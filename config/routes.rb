5035cfd17b9ef90dcacd394d72d3c7bf6e30025b
tana1818 <tanakagen1124@gmail.com>
2019-03-01 21:13:41 +0900

    Sider edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # resources :users
-  resources :users, only:[:edit, :show, :index]
+  resources :users
   resources :gen
 end

7275706e75f601b283cf1144406fb7d70f23e0d4
teru0421 <kazuhiro.terui4036@gmail.com>
2019-03-01 17:47:22 +0900

    Correction

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
-
-  root 'users#login'
+  root 'users#show'
+  resources :users, only: [:show]
   get  '/users' => 'users#login'
   get  '/users/logout' => 'users#logout'
   get  '/users/show' => 'users#show'
   get  '/users/credit' => 'users#credit'
   get  '/users/creditcreate' => 'users#creditcreate'
 
-=======
-  resources :users, only: [:show]
-  root 'users#show'
->>>>>>> master
 end

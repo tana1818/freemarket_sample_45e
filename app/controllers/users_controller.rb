307999c06ddf2081a901562b65744908c53bf60a
teru0421 <kazuhiro.terui4036@gmail.com>
2019-03-04 20:38:54 +0900

    markup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,3 @@
 class UsersController < ApplicationController
 
-  def login
-  end
-
-  def logout
-  end
-
-  def credit
-  end
-
-  def creditcreate
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#show'
-  resources :users, only: [:show]
-  get  '/users' => 'users#login'
-  get  '/users/logout' => 'users#logout'
   get  '/users/show' => 'users#show'
+  get  '/users/login' => 'users#login'
+  get  '/users/logout' => 'users#logout'
   get  '/users/credit' => 'users#credit'
-  get  '/users/creditcreate' => 'users#creditcreate'
-
+  get  '/users/credit_create' => 'users#credit_create'
+  get  '/users/signup' => 'users#signup'
+  get  '/users/signup_memberinfo' => 'users#signup_memberinfo'
+  get  '/users/signup_tel' => 'users#signup_tel'
+  get  '/users/signup_address' => 'users#signup_address'
+  get  '/users/signup_pay' => 'users#signup_pay'
+  get  '/users/signup_complete' => 'users#signup_complete'
 end

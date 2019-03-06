2b9fae76d7fd91c0316a57450b34951504d21ea4
kozitex <47021855+kozitex@users.noreply.github.com>
2019-03-06 11:29:15 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#show'
   resources :users, only: [:show]
+  resources :user_details, only: [:index]
   
 end

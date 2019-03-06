809c0c5efbaca6d01f7c6ae0e2834c19410b79e7
tana1818 <tanakagen1124@gmail.com>
2019-03-06 19:37:48 +0900

    商品詳細　修正３

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  resources :users
-  resources :products
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#show'
+  resources :products, onry: [:show]
   resources :users, only: [:show, :new]
   resources :user_details, only: [:index]
-  
+
 end

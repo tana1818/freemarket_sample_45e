216869491af023f808025f3cd0905201792054db
teru0421 <kazuhiro.terui4036@gmail.com>
2019-03-08 19:41:37 +0900

    Merge branch 'kozitex/master'
    # Conflicts:
    #	config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users
   root 'products#index'
-  resources :products, only: [:index, :show, :new]
+  resources :products, only: [:index, :show, :new] do
+    collection do
+      get :purchase_confirmation
+    end
+  end
   resources :user_details, only: [:index]
   resources :users, only: [:show, :new] do
     collection do
@@ -17,4 +23,5 @@ Rails.application.routes.draw do
       get :signup_complete
     end
   end
+
 end

cc9e6847e17ebb14fbaaffcd6a99c69a6da50d4e
kozitex <47021855+kozitex@users.noreply.github.com>
2019-03-08 15:17:55 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 root 'products#index'
-resources :products, only: [:index, :show, :new]
+resources :products, only: [:index, :show, :new] do
+  collection do
+    get :purchase-confirmation
+  end
+end
 resources :user_details, only: [:index]
 resources :users, only: [:show, :new] do
   collection do

1f103892ea48e68332f119c0795afcfc5deffd63
kozitex <grassrunners@gmail.com>
2019-03-14 20:55:09 +0900

    Fix naming convention to BEM

--- app/assets/javascripts/dropzone.js
+++ app/assets/javascripts/dropzone.js
@@ -567,14 +567,14 @@ var Dropzone = function (_Emitter) {
 
             var child = _ref2;
 
-            if (/(^| )dz-message($| )/.test(child.className)) {
+            if (/(^| )dropzone__message($| )/.test(child.className)) {
               messageElement = child;
-              child.className = "dz-message"; // Removes the 'dz-default' class
+              child.className = "dropzone__message"; // Removes the 'dz-default' class
               break;
             }
           }
           if (!messageElement) {
-            messageElement = Dropzone.createElement("<div class=\"dz-message\"><span></span></div>");
+            messageElement = Dropzone.createElement("<div class=\"dropzone__message\"><span></span></div>");
             this.element.appendChild(messageElement);
           }
 
@@ -693,7 +693,7 @@ var Dropzone = function (_Emitter) {
          *       .innerHTML
          *
          */
-        previewTemplate: "<div class=\"dz-preview dz-file-preview\">\n  <div class=\"dz-image\"><img data-dz-thumbnail /></div>\n  <div class=\"dz-details\">\n    <div class=\"dz-size\"><span data-dz-size></span></div>\n    <div class=\"dz-filename\"><span data-dz-name></span></div>\n  </div>\n  <div class=\"dz-progress\"><span class=\"dz-upload\" data-dz-uploadprogress></span></div>\n  <div class=\"dz-error-message\"><span data-dz-errormessage></span></div>\n  <div class=\"dz-success-mark\">\n    <svg width=\"54px\" height=\"54px\" viewBox=\"0 0 54 54\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:sketch=\"http://www.bohemiancoding.com/sketch/ns\">\n      <title>Check</title>\n      <defs></defs>\n      <g id=\"Page-1\" stroke=\"none\" stroke-width=\"1\" fill=\"none\" fill-rule=\"evenodd\" sketch:type=\"MSPage\">\n        <path d=\"M23.5,31.8431458 L17.5852419,25.9283877 C16.0248253,24.3679711 13.4910294,24.366835 11.9289322,25.9289322 C10.3700136,27.4878508 10.3665912,30.0234455 11.9283877,31.5852419 L20.4147581,40.0716123 C20.5133999,40.1702541 20.6159315,40.2626649 20.7218615,40.3488435 C22.2835669,41.8725651 24.794234,41.8626202 26.3461564,40.3106978 L43.3106978,23.3461564 C44.8771021,21.7797521 44.8758057,19.2483887 43.3137085,17.6862915 C41.7547899,16.1273729 39.2176035,16.1255422 37.6538436,17.6893022 L23.5,31.8431458 Z M27,53 C41.3594035,53 53,41.3594035 53,27 C53,12.6405965 41.3594035,1 27,1 C12.6405965,1 1,12.6405965 1,27 C1,41.3594035 12.6405965,53 27,53 Z\" id=\"Oval-2\" stroke-opacity=\"0.198794158\" stroke=\"#747474\" fill-opacity=\"0.816519475\" fill=\"#FFFFFF\" sketch:type=\"MSShapeGroup\"></path>\n      </g>\n    </svg>\n  </div>\n  <div class=\"dz-error-mark\">\n    <svg width=\"54px\" height=\"54px\" viewBox=\"0 0 54 54\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:sketch=\"http://www.bohemiancoding.com/sketch/ns\">\n      <title>Error</title>\n      <defs></defs>\n      <g id=\"Page-1\" stroke=\"none\" stroke-width=\"1\" fill=\"none\" fill-rule=\"evenodd\" sketch:type=\"MSPage\">\n        <g id=\"Check-+-Oval-2\" sketch:type=\"MSLayerGroup\" stroke=\"#747474\" stroke-opacity=\"0.198794158\" fill=\"#FFFFFF\" fill-opacity=\"0.816519475\">\n          <path d=\"M32.6568542,29 L38.3106978,23.3461564 C39.8771021,21.7797521 39.8758057,19.2483887 38.3137085,17.6862915 C36.7547899,16.1273729 34.2176035,16.1255422 32.6538436,17.6893022 L27,23.3431458 L21.3461564,17.6893022 C19.7823965,16.1255422 17.2452101,16.1273729 15.6862915,17.6862915 C14.1241943,19.2483887 14.1228979,21.7797521 15.6893022,23.3461564 L21.3431458,29 L15.6893022,34.6538436 C14.1228979,36.2202479 14.1241943,38.7516113 15.6862915,40.3137085 C17.2452101,41.8726271 19.7823965,41.8744578 21.3461564,40.3106978 L27,34.6568542 L32.6538436,40.3106978 C34.2176035,41.8744578 36.7547899,41.8726271 38.3137085,40.3137085 C39.8758057,38.7516113 39.8771021,36.2202479 38.3106978,34.6538436 L32.6568542,29 Z M27,53 C41.3594035,53 53,41.3594035 53,27 C53,12.6405965 41.3594035,1 27,1 C12.6405965,1 1,12.6405965 1,27 C1,41.3594035 12.6405965,53 27,53 Z\" id=\"Oval-2\" sketch:type=\"MSShapeGroup\"></path>\n        </g>\n      </g>\n    </svg>\n  </div>\n</div>",
+        previewTemplate: "<div class=\"dropzone__preview dz-file-preview\">\n  <div class=\"dropzone__image\"><img data-dz-thumbnail /></div>\n  <div class=\"dropzone__details\">\n    <div class=\"dropzone__size\"><span data-dz-size></span></div>\n    <div class=\"dropzone__filename\"><span data-dz-name></span></div>\n  </div>\n  <div class=\"dropzone__progress\"><span class=\"dz-upload\" data-dz-uploadprogress></span></div>\n  <div class=\"dropzone__error-message\"><span data-dz-errormessage></span></div>\n  <div class=\"dropzone__success-mark\">\n    <svg width=\"54px\" height=\"54px\" viewBox=\"0 0 54 54\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:sketch=\"http://www.bohemiancoding.com/sketch/ns\">\n      <title>Check</title>\n      <defs></defs>\n      <g id=\"Page-1\" stroke=\"none\" stroke-width=\"1\" fill=\"none\" fill-rule=\"evenodd\" sketch:type=\"MSPage\">\n        <path d=\"M23.5,31.8431458 L17.5852419,25.9283877 C16.0248253,24.3679711 13.4910294,24.366835 11.9289322,25.9289322 C10.3700136,27.4878508 10.3665912,30.0234455 11.9283877,31.5852419 L20.4147581,40.0716123 C20.5133999,40.1702541 20.6159315,40.2626649 20.7218615,40.3488435 C22.2835669,41.8725651 24.794234,41.8626202 26.3461564,40.3106978 L43.3106978,23.3461564 C44.8771021,21.7797521 44.8758057,19.2483887 43.3137085,17.6862915 C41.7547899,16.1273729 39.2176035,16.1255422 37.6538436,17.6893022 L23.5,31.8431458 Z M27,53 C41.3594035,53 53,41.3594035 53,27 C53,12.6405965 41.3594035,1 27,1 C12.6405965,1 1,12.6405965 1,27 C1,41.3594035 12.6405965,53 27,53 Z\" id=\"Oval-2\" stroke-opacity=\"0.198794158\" stroke=\"#747474\" fill-opacity=\"0.816519475\" fill=\"#FFFFFF\" sketch:type=\"MSShapeGroup\"></path>\n      </g>\n    </svg>\n  </div>\n  <div class=\"dropzone__error-mark\">\n    <svg width=\"54px\" height=\"54px\" viewBox=\"0 0 54 54\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:sketch=\"http://www.bohemiancoding.com/sketch/ns\">\n      <title>Error</title>\n      <defs></defs>\n      <g id=\"Page-1\" stroke=\"none\" stroke-width=\"1\" fill=\"none\" fill-rule=\"evenodd\" sketch:type=\"MSPage\">\n        <g id=\"Check-+-Oval-2\" sketch:type=\"MSLayerGroup\" stroke=\"#747474\" stroke-opacity=\"0.198794158\" fill=\"#FFFFFF\" fill-opacity=\"0.816519475\">\n          <path d=\"M32.6568542,29 L38.3106978,23.3461564 C39.8771021,21.7797521 39.8758057,19.2483887 38.3137085,17.6862915 C36.7547899,16.1273729 34.2176035,16.1255422 32.6538436,17.6893022 L27,23.3431458 L21.3461564,17.6893022 C19.7823965,16.1255422 17.2452101,16.1273729 15.6862915,17.6862915 C14.1241943,19.2483887 14.1228979,21.7797521 15.6893022,23.3461564 L21.3431458,29 L15.6893022,34.6538436 C14.1228979,36.2202479 14.1241943,38.7516113 15.6862915,40.3137085 C17.2452101,41.8726271 19.7823965,41.8744578 21.3461564,40.3106978 L27,34.6568542 L32.6538436,40.3106978 C34.2176035,41.8744578 36.7547899,41.8726271 38.3137085,40.3137085 C39.8758057,38.7516113 39.8771021,36.2202479 38.3106978,34.6538436 L32.6568542,29 Z M27,53 C41.3594035,53 53,41.3594035 53,27 C53,12.6405965 41.3594035,1 27,1 C12.6405965,1 1,12.6405965 1,27 C1,41.3594035 12.6405965,53 27,53 Z\" id=\"Oval-2\" sketch:type=\"MSShapeGroup\"></path>\n        </g>\n      </g>\n    </svg>\n  </div>\n</div>",
 
         // END OPTIONS
         // (Required by the dropzone documentation parser)
@@ -710,20 +710,20 @@ var Dropzone = function (_Emitter) {
 
         // Those are self explanatory and simply concern the DragnDrop.
         drop: function drop(e) {
-          return this.element.classList.remove("dz-drag-hover");
+          return this.element.classList.remove("dropzone__drag-hover");
         },
         dragstart: function dragstart(e) {},
         dragend: function dragend(e) {
-          return this.element.classList.remove("dz-drag-hover");
+          return this.element.classList.remove("dropzone__drag-hover");
         },
         dragenter: function dragenter(e) {
-          return this.element.classList.add("dz-drag-hover");
+          return this.element.classList.add("dropzone__drag-hover");
         },
         dragover: function dragover(e) {
-          return this.element.classList.add("dz-drag-hover");
+          return this.element.classList.add("dropzone__drag-hover");
         },
         dragleave: function dragleave(e) {
-          return this.element.classList.remove("dz-drag-hover");
+          return this.element.classList.remove("dropzone__drag-hover");
         },
         paste: function paste(e) {},
 
@@ -783,7 +783,7 @@ var Dropzone = function (_Emitter) {
 
             // 削除ボタンの隣に編集ボタンを追加
             if (this.options.addRemoveLinks) {
-              file._removeLink = Dropzone.createElement("<div class=\"dz-link\"><a class=\"dz-remove\">編集</a><a class=\"dz-remove\" href=\"javascript:undefined;\" data-dz-remove>" + this.options.dictRemoveFile + "</a></div>");
+              file._removeLink = Dropzone.createElement("<div class=\"dropzone__link\"><a class=\"dropzone__remove\">編集</a><a class=\"dropzone__remove\" href=\"javascript:undefined;\" data-dz-remove>" + this.options.dictRemoveFile + "</a></div>");
               file.previewElement.appendChild(file._removeLink);
             }
 
@@ -1205,8 +1205,8 @@ var Dropzone = function (_Emitter) {
         this.element.setAttribute("enctype", "multipart/form-data");
       }
 
-      if (this.element.classList.contains("dropzone") && !this.element.querySelector(".dz-message")) {
-        this.element.appendChild(Dropzone.createElement("<div class=\"dz-default dz-message\"><span>" + this.options.dictDefaultMessage + "</span></div>"));
+      if (this.element.classList.contains("dropzone") && !this.element.querySelector(".dropzone__message")) {
+        this.element.appendChild(Dropzone.createElement("<div class=\"dz-default dropzone__message\"><span>" + this.options.dictDefaultMessage + "</span></div>"));
       }
 
       if (this.clickableElements.length) {
@@ -1365,7 +1365,7 @@ var Dropzone = function (_Emitter) {
           events: {
             "click": function click(evt) {
               // Only the actual dropzone or the message element should trigger file selection
-              if (clickableElement !== _this3.element || evt.target === _this3.element || Dropzone.elementInside(evt.target, _this3.element.querySelector(".dz-message"))) {
+              if (clickableElement !== _this3.element || evt.target === _this3.element || Dropzone.elementInside(evt.target, _this3.element.querySelector(".dropzone__message"))) {
                 _this3.hiddenFileInput.click(); // Forward the click
               }
               return true;
@@ -1826,7 +1826,7 @@ var Dropzone = function (_Emitter) {
 
       // ドロップボックスのファイル数が4個を超えたら、ドロップボックスの大きさを拡張する
       if (this.getAcceptedFiles().length >= 4) {
-        $('.dropzone-previews').addClass('preview-extension');
+        $('.dropzone__previews').addClass('dropzone__preview-exte');
       }
 
       file.upload = {
@@ -1944,7 +1944,7 @@ var Dropzone = function (_Emitter) {
 
       //ドロップボックスのファイルの数が4つ以下になったら、ドロップボックスのサイズを小さくする
       if (this.getAcceptedFiles().length <= 4) {
-        $('.dropzone-previews').removeClass('preview-extension');
+        $('.dropzone__previews').removeClass('dropzone__preview-exte');
       }
 
       this.emit("removedfile", file);

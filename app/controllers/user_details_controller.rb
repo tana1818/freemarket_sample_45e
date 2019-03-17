c414aede1e0ce024c1020ad78bc3080549d99196
nekozestrtgy <nekozestrtgy@gmail.com>
2019-03-17 16:29:43 +0900

    🎨 : Fix initial code in controller

--- app/controllers/user_details_controller.rb
+++ app/controllers/user_details_controller.rb
@@ -1,7 +1,7 @@
 class UserDetailsController < ApplicationController
 
   def index  #本人確認ページ。仮で作成中。
-    @user = User.find(1)
+    @user = User.find(params[:id])
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,26 @@
 class UsersController < ApplicationController
 
   def new # 仮のidで作成中
-    @user = User.find(1)
+    @user = User.find(params[:id])
   end
 
   def create
   end
 
   def show # 仮のidで作成中
-    @user = User.find(1)
+    @user = User.find(params[:id])
   end
 
   def credit # 仮のidで作成中
-    @user = User.find(1)
+    @user = User.find(params[:id])
   end
 
   def credit_create # 仮のidで作成中
-    @user = User.find(1)
+    @user = User.find(params[:id])
   end
 
   def logout # 仮のidで作成中
-    @user = User.find(1)
+    @user = User.find(params[:id])
   end
 
 end

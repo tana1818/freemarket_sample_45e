a009875caaf096d6b5b9e3ae1b2e7631a28af856
nekozestrtgy <nekozestrtgy@gmail.com>
2019-03-17 15:32:45 +0900

    ✨ : Add new function of breadcrumbs
    パンくずの作成

--- app/controllers/user_details_controller.rb
+++ app/controllers/user_details_controller.rb
@@ -1,6 +1,7 @@
 class UserDetailsController < ApplicationController
 
-  def index  #本人確認ページ
+  def index  #本人確認ページ。仮で作成中。
+    @user = User.find(1)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,26 @@
 class UsersController < ApplicationController
 
-  def new
+  def new # 仮のidで作成中
+    @user = User.find(1)
   end
 
   def create
+  end
+
+  def show # 仮のidで作成中
+    @user = User.find(1)
+  end
+
+  def credit # 仮のidで作成中
+    @user = User.find(1)
+  end
+
+  def credit_create # 仮のidで作成中
+    @user = User.find(1)
+  end
+
+  def logout # 仮のidで作成中
+    @user = User.find(1)
   end
 
 end

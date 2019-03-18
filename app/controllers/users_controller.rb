901498b5e0f4ee790f15d71490d7423c5f3ab56d
nekozestrtgy <nekozestrtgy@gmail.com>
2019-03-18 19:00:50 +0900

    🐛 : Fix wrong code in controllers about breadcrumbs
    パンくずに関するコントローラーの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,26 @@
 class UsersController < ApplicationController
 
   def new
-    @user = User.find(params[:id])
+    @user = User.find(current_user.id)
   end
 
   def create
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find(current_user.id)
   end
 
   def credit
-    @user = User.find(params[:id])
+    @user = User.find(current_user.id)
   end
 
   def credit_create
-    @user = User.find(params[:id])
+    @user = User.find(current_user.id)
   end
 
   def logout
-    @user = User.find(params[:id])
+    @user = User.find(current_user.id)
   end
 
 end

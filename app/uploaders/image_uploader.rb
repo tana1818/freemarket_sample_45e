da4457826c1d44148820f13e4ba2100682018a2a
tana1818 <tanakagen1124@gmail.com>
2019-05-06 19:26:30 +0900

    Fix to s3

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,15 +7,15 @@ class ImageUploader < CarrierWave::Uploader::Base
   process resize_to_fit: [114, 114]
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+  storage :fog
 
   if Rails.env.development?
     storage :file
   elsif Rails.env.test?
     storage :file
   else
-    # storage :fog
+    storage :fog
   end
 
   # Override the directory where uploaded files will be stored.

784004cea438b40649e5c212d4c44337a7a766d5
tana1818 <tanakagen1124@gmail.com>
2019-07-11 21:35:55 +0900

    Fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_one :deliverie, dependent: :destroy
   accepts_nested_attributes_for :deliverie
   has_one :purchase, dependent: :destroy
-  has_one :card, dependent: :destroy
+  # has_one :card, dependent: :destroy
   # accepts_nested_attributes_for :card
   has_many :products
   has_many :comments

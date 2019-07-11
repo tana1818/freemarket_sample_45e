48a85916260f2e3ea4c51d2b7565b79a575ab799
tana1818 <tanakagen1124@gmail.com>
2019-07-11 21:33:00 +0900

    Fix

--- db/migrate/20190306100122_create_cards.rb
+++ db/migrate/20190306100122_create_cards.rb
@@ -5,7 +5,7 @@ class CreateCards < ActiveRecord::Migration[5.2]
       t.integer :expiration_month, null: false
       t.integer :expiration_year, null: false
       t.integer :security_code, null: false
-      # t.references :user, foreign_key: true
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end

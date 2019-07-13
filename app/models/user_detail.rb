26d6438624c33cce3164eb31637a97e862f9ef58
tana1818 <tanakagen1124@gmail.com>
2019-07-13 12:03:07 +0900

    Fix

--- app/models/user_detail.rb
+++ app/models/user_detail.rb
@@ -2,6 +2,7 @@ class UserDetail < ApplicationRecord
   belongs_to :user, optional: true
   has_many :prerfectures
 
-  validates :auth_tel, presence: true,numericality: true,length: { maximum: 11 }
+  # validates :auth_tel, presence: true,numericality: true,length: { maximum: 11 }
+  validates :auth_tel, presence: true
 
 end
